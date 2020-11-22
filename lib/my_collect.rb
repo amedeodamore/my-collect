cbb9a2e02824efb06d90c04d15a5aee25c2c1a65
Amedeo d'Amore <amedeodamore@gmail.com>
2020-11-22 15:21:04 +0000

    commit tree 3ae0d8ba0d0f26976851713f6247805fbdb264bb

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
 
+def my_collect(array)
+    i = 0
+    collection = []
+    while i < array.length
+      collection << yield(array[i])
+     i += 1
+    end
 
+    array.collect do |language|
+        "#{language.upcase}"
+    end
+      array.collect do |names|
+        "#{names.split(" ").first}"
+    end
+    return collection
+  end 
+
+ # it "returns a new collection of appropriately modified elements, in this case capitalized languages" do
+ #   expect(my_collect(languages) do |language|
+ #     language.upcase
+ #   end).to eq(["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"])
+ # end
